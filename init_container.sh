1904b18b758d8127ffab7ed69d3776b19ec816fe
Srikanth Sureddy <sureddy1@outlook.com>
2017-08-29 10:53:52 -0500

    Fix for storage error

--- init_container.sh
+++ init_container.sh
@@ -255,7 +255,6 @@ EOPHP
 		if [ ! -d "/home/site/html/wp-content/plugins/windows-azure-storage" ]; then
 			echo >&2 "WP Azure Storage Plugin is not installed. Installing it"		
 			wp --allow-root --path=/var/www/html/ plugin install --activate /tmp/wp-azure-storage.zip
-			wp plugin --activate windows-azure-storage --allow-root
 			wp db query 'insert into wp_options values (0,"azure_storage_account_name","$StorageAccountBlobUrl","yes")' --allow-root
 			wp db query 'insert into wp_options values (0,"azure_storage_account_primary_access_key","$StorageAccountKey","yes")' --allow-root
 			wp windows-azure-storage container-create wpstorage --allow-root
