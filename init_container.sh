228976637f6f099c7fd87a143d4291b4310b9326
Srikanth Sureddy <sureddy1@outlook.com>
2017-09-06 12:37:51 -0500

    Check for Wp Core Install

--- init_container.sh
+++ init_container.sh
@@ -216,13 +216,13 @@ $mysql->close();
 EOPHP
 	fi
 
-	#if ! $(wp core --allow-root --path=/var/www/html/ is-installed); then
+	if ! $(wp core --allow-root --path=/var/www/html/ is-installed); then
 		if [ ! d "/home/site/wwwroot/wp-content" ]; then
 			echo >&2 "WP Core is not installed. Installing WP Core"
 			wp --allow-root --path=/var/www/html/ core install --url="$WORDPRESS_URL" --title="$WORDPRESS_TITLE" --admin_user="$WORDPRESS_ADMINUSER" --admin_password="$WORDPRESS_ADMINPASSWORD" --admin_email="$WORDPRESS_ADMINEMAIL"
 			echo >&2 "WP Core installed successfully"
 		fi
-	#fi
+	fi
 	
 	#if ! $(wp plugin --allow-root --path=/var/www/html/  is-installed windows-azure-storage); then
 		if [ ! -d "/home/site/wwwroot/wp-content/plugins/windows-azure-storage" ]; then
